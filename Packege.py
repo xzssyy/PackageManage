f00a74208ca99835677e0f96bd49a1fb291030a2
xzssyy <xzssyy@gmail.com>
2024-12-22 16:00:24 +0800

    完成package的类封装

--- Packege.py
+++ Packege.py
@@ -1,64 +1,62 @@
 class Package:
-    """
 
-    """
-
-
-
-    def __init__(self, versionString):
+    def __init__(self, version_string):
         """
 
-        :param args:
+        Args:
+            version_string: numpy>=1.1.1
         """
-        # versionList = [name, (symbol), versionNumber1, versionNumber2, ...]
-        self.remark, self.isPontificated = self.convertVsersionAsList(versionString)
-        self.name = self.remark[0]
-        self.symbol = None
-
-        if self.isPontificated:
-            self.symbol = self.remark[1]
-            self.versionList = self.remark[2:]
-        else:
-            self.versionList = self.remark[1:]
+        # version_list = [name, (symbol), versionNumber1, versionNumber2, ...]
+        self.version_list = self.convert_version_as_list(version_string)
+        self.name = self.version_list[0]
+        self.symbol = self.version_list[1]
+        self.version_number_list = self.version_list[2:]
 
 
+    def __eq__(self, other):
+        if isinstance(other, Package):
+            return self.name == other.name
+        return False
 
 
-    def getVersionString(self):
-        return'.'.join(self.versionList)
-
-
-
-
-
-
-
-    def convertVersionAsList(versionString):
-        versinAsList = versionString.name.split('.')
-
-        symbol=["==", '<=', '>=']
-        isPontificated = False
-
-        for s in symbol:
-            if s in versinAsList[0]:
-                t = versinAsList[0].split(s)
-                t.extend(versinAsList)
-                versinAsList = t
-                isPontificated = True
-                break
-
-        return versinAsList, isPontificated
+    def get_version_as_string(self):
+        return '.'.join(self.version_number)
 
 
     @classmethod
-    def versionCmp(cls, v1, v2):
+    def convert_version_as_list(cls, version_string):
+        """
+        将记录转换为字符串的列表
+        Args:
+            version_string: numpy>=1.1.1
+
+        Returns:
+            [package_name, symbol, version_number1, version_number2, ...]
+        """
+        version_as_list = version_string.name.split('.')
+
+        symbol_list=["==", '<=', '>=']
+
+        for s in symbol_list:
+            if s in version_as_list[0]:
+                t = version_as_list[0].split(s)
+                return [*t, *version_as_list[1:]]
+
+        version_as_list = [version_as_list[0], None, *version_as_list[1]]
+        return version_as_list
+
+
+    @classmethod
+    def version_cmp(cls, v1, v2):
         if v1[0] == v2[0]:
-            return Package.versionCmp(v1[1:], v2[1:])
+            return Package.version_cmp(v1[1:], v2[1:])
 
         if v1[0] > v2[0]:
             return 1
         else:
             return 0
+
+
 
 
 
