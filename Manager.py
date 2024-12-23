9f5a4974bff4ce2a390229fc2a6fbed03297a435
xzssyy <xzssyy@gmail.com>
2024-12-23 15:21:20 +0800

    1223

--- Manager.py
+++ Manager.py
@@ -9,27 +9,41 @@ class Manager:
     """
 
     def __init__(self, custom_node_path):
+        """
+        保存数据结构
+        节点名称序号映射表， 包名称序号映射表， 节点序号与包序号的关系图
+        Args:
+            custom_node_path: 
+        """
         self.root = Path(custom_node_path)
 
-        # {"pack_name":[(node_name, )]}
-        self.data = {}
-        # {"pack_bane":"base_version"}
-        self.packages_need = {}
-        nodes_name = [folder.name for folder in self.root.iterdir() if folder.is_dir()]
+        self.nodes = []
+        self.nodes_idx = None
+        self.packages = []
+        self.nodes_num = 0
+        self.packages_num = 0
+        # 初始化为100*1000的数组
+        self.np_map = np.zeros((100, 1000))
+        self.read_data()
 
 
 
 
-    def judge_conflict(self, base_package, package):
+    def read_data(self):
+        self.
+        self.nodes_idx = {folder.name : idx for idx, folder in enumerate(self.root.iterdir()) if folder.is_dir()}
+        for node_name, _ in self.nodes.items():
+            self.read_node_packages(node_name)
 
 
 
 
+    def read_node_packages(self, node_name):
 
-    def read_data(self, node_name):
-        self.data[node_name] = []
         req_path = Path(self.root, node_name, "requirements.txt")
-        package_list = []
+        if not req_path.exists():
+            return
+
         with open(req_path, "r", encoding="utf-8") as f:
             lines = f.readlines()
             for line in lines:
@@ -37,7 +51,24 @@ class Manager:
 
 
 
+    def judge_conflict(self, base_package, package):
+        pass
+
+
+
+
+
+
+
     def modify_req_file(self, node_name):
+        """
+        修改对应node的requirements文件
+        Args:
+            node_name: 
+
+        Returns:
+
+        """
         with open(Path(self.root, node_name, "requirements.txt"), "r", encoding="utf-8") as f:
             lines = f.readlines()
             for line in lines:
